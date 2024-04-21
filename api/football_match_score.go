087944919c8986ac32dd9c232912b22918837db8
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-04-21 10:18:52 +0530

    added the update football score functionality

--- api/football_match_score.go
+++ api/football_match_score.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	db "khelogames/db/sqlc"
 	"net/http"
 	"strconv"
@@ -8,40 +9,59 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type addFootballMatchScoreRequest struct {
+	MatchID      int64 `json:"match_id"`
+	TournamentID int64 `json:"tournament_id"`
+	TeamID       int64 `json:"team_id"`
+	GoalScore    int64 `json:"goal_score"`
+}
+
 func (server *Server) addFootballMatchScore(ctx *gin.Context) {
-	matchIdStr := ctx.Query("match_id")
-	matchId, err := strconv.ParseInt(matchIdStr, 10, 64)
+
+	var req addFootballMatchScoreRequest
+	err := ctx.ShouldBindJSON(&req)
 	if err != nil {
-		ctx.Error(err)
+		ctx.JSON(http.StatusInternalServerError, err)
 		return
 	}
 
-	tournamentIdStr := ctx.Query("tournament_id")
-	tournamentId, err := strconv.ParseInt(tournamentIdStr, 10, 64)
-	if err != nil {
-		ctx.Error(err)
-		return
-	}
+	// matchIdStr := ctx.Query("match_id")
+	// matchId, err := strconv.ParseInt(matchIdStr, 10, 64)
+	// if err != nil {
+	// 	fmt.Println("Line no 15: ", err)
+	// 	ctx.Error(err)
+	// 	return
+	// }
 
-	teamIdStr := ctx.Query("team_id")
-	teamID, err := strconv.ParseInt(teamIdStr, 10, 64)
-	if err != nil {
-		ctx.Error(err)
-		return
-	}
+	// tournamentIdStr := ctx.Query("tournament_id")
+	// tournamentId, err := strconv.ParseInt(tournamentIdStr, 10, 64)
+	// if err != nil {
+	// 	fmt.Println("Line no 24: ", err)
+	// 	ctx.Error(err)
+	// 	return
+	// }
 
-	goalScoreStr := ctx.Query("goal_score")
-	goalScore, err := strconv.ParseInt(goalScoreStr, 10, 64)
-	if err != nil {
-		ctx.Error(err)
-		return
-	}
+	// teamIdStr := ctx.Query("team_id")
+	// teamID, err := strconv.ParseInt(teamIdStr, 10, 64)
+	// if err != nil {
+	// 	fmt.Println("Line no 32: ", err)
+	// 	ctx.Error(err)
+	// 	return
+	// }
+
+	// goalScoreStr := ctx.Query("goal_score")
+	// goalScore, err := strconv.ParseInt(goalScoreStr, 10, 64)
+	// if err != nil {
+	// 	fmt.Println("Line no 40: ", err)
+	// 	ctx.Error(err)
+	// 	return
+	// }
 
 	arg := db.AddFootballMatchScoreParams{
-		MatchID:      matchId,
-		TournamentID: tournamentId,
-		TeamID:       teamID,
-		GoalScore:    goalScore,
+		MatchID:      req.MatchID,
+		TournamentID: req.TournamentID,
+		TeamID:       req.TeamID,
+		GoalScore:    req.GoalScore,
 	}
 
 	response, err := server.store.AddFootballMatchScore(ctx, arg)
@@ -84,4 +104,55 @@ func (server *Server) getFootballMatchScore(ctx *gin.Context) {
 	ctx.JSON(http.StatusAccepted, response)
 	return
 
+}
+
+func (server *Server) updateFootballMatchScore(ctx *gin.Context) {
+	tournamentIdStr := ctx.Query("tournament_id")
+	matchIdStr := ctx.Query("match_id")
+	teamIdStr := ctx.Query("team_id")
+	goalScoreStr := ctx.Query("goal_score")
+	matchID, err := strconv.ParseInt(matchIdStr, 10, 64)
+	if err != nil {
+		fmt.Errorf("unable to parse the match Id")
+		ctx.JSON(http.StatusResetContent, err)
+		return
+	}
+
+	teamID, err := strconv.ParseInt(teamIdStr, 10, 64)
+	if err != nil {
+		fmt.Errorf("unable to parse the match Id")
+		ctx.JSON(http.StatusResetContent, err)
+		return
+	}
+
+	goalScore, err := strconv.ParseInt(goalScoreStr, 10, 64)
+	if err != nil {
+		fmt.Errorf("unable to parse the match Id")
+		ctx.JSON(http.StatusResetContent, err)
+		return
+	}
+
+	tournamentId, err := strconv.ParseInt(tournamentIdStr, 10, 64)
+	if err != nil {
+		fmt.Errorf("unable to parse the match Id")
+		ctx.JSON(http.StatusResetContent, err)
+		return
+	}
+
+	arg := db.UpdateFootballMatchScoreParams{
+		TournamentID: tournamentId,
+		MatchID:      matchID,
+		TeamID:       teamID,
+		GoalScore:    goalScore,
+	}
+
+	response, err := server.store.UpdateFootballMatchScore(ctx, arg)
+	if err != nil {
+		fmt.Errorf("unable to get the response ")
+		ctx.JSON(http.StatusResetContent, err)
+		return
+	}
+
+	ctx.JSON(http.StatusAccepted, response)
+	return
 }
