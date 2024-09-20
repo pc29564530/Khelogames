816664fc97e7cf4ccbe1a8c2722592dd20c2659b
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-09-20 14:29:46 +0530

    combine the line up and substitution in on efile

--- api/sports/football/football_lineup.go
+++ api/sports/football/football_lineup.go
@@ -80,11 +80,6 @@ func (s *FootballServer) UpdateFootballSubsAndLineUpFunc(ctx *gin.Context) {
 		return
 	}
 
-	// arg := db.UpdateFootballLineUpParams{
-	// 	ID: req.LineUpID,
-	// 	PlayerID: req.PlayerID
-	// }
-
 	arg := db.UpdateFootballSubsAndLineUpParams{
 		ID:   req.LineUpID,
 		ID_2: req.SubsID,
@@ -93,6 +88,68 @@ func (s *FootballServer) UpdateFootballSubsAndLineUpFunc(ctx *gin.Context) {
 	response, err := s.store.UpdateFootballSubsAndLineUp(ctx, arg)
 	if err != nil {
 		s.logger.Error("Failed to update the football and Subs")
+		return
+	}
+
+	ctx.JSON(http.StatusAccepted, response)
+}
+
+// substitution code
+
+type addSubsRequest struct {
+	TeamID   int64  `json:"team_id"`
+	PlayerID int64  `json:"player_id"`
+	MatchID  int64  `json:"match_id"`
+	Position string `json:"position"`
+}
+
+func (s *FootballServer) AddFootballSubstitionFunc(ctx *gin.Context) {
+	var req addSubsRequest
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		s.logger.Error("Failed to bind: ", err)
+		return
+	}
+
+	arg := db.AddFootballSubstitutionParams{
+		TeamID:   req.TeamID,
+		PlayerID: req.PlayerID,
+		MatchID:  req.MatchID,
+		Position: req.Position,
+	}
+
+	response, err := s.store.AddFootballSubstitution(ctx, arg)
+	if err != nil {
+		s.logger.Error("Failed to add the player in lineup: ", err)
+		return
+	}
+
+	ctx.JSON(http.StatusAccepted, response)
+}
+
+type getSubstitutionpRequest struct {
+	TeamID   int64  `json:"team_id"`
+	PlayerID int64  `json:"player_id"`
+	MatchID  int64  `json:"match_id"`
+	Position string `json:"position"`
+}
+
+func (s *FootballServer) GetFootballSubstitutionFunc(ctx *gin.Context) {
+	var req getSubstitutionpRequest
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		s.logger.Error("Failed to bind: ", err)
+		return
+	}
+
+	arg := db.GetFootballSubstitutionParams{
+		TeamID:  req.TeamID,
+		MatchID: req.MatchID,
+	}
+
+	response, err := s.store.GetFootballSubstitution(ctx, arg)
+	if err != nil {
+		s.logger.Error("Failed to get the player in lineup: ", err)
 		return
 	}
 
