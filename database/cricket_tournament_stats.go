fb4cb45e6d8693afefce83b614de9d211bd519b7
Pawan <pc29564530@gmail.com>
2025-05-12 18:08:58 +0530

    Added the fifties and hundreds functionality

--- database/cricket_tournament_stats.go
+++ database/cricket_tournament_stats.go
@@ -6,57 +6,60 @@ import (
 	"fmt"
 )
 
-const cricketTournamentsStats = `
-	SELECT
-		p.id AS player_id,
-		p.player_name AS player_name,
-		tm.name AS team_name,
-		SUM(b.runs_scored) AS total_runs
-	FROM bats b
-	JOIN matches m ON m.id = b.match_id
-	JOIN players p ON p.id = b.batsman_id
-	JOIN teams tm ON tm.id = b.team_id
-	WHERE m.tournament_id = $1
-	GROUP BY p.id, p.player_name, tm.name
-	ORDER BY total_runs DESC;
-`
-
-type mostRun struct {
+type PlayerStat struct {
 	PlayerID   int64  `json:"player_id"`
 	PlayerName string `json:"player_name"`
 	TeamName   string `json:"team_name"`
-	TotalRuns  string `json:"total_runs"`
+	StatValue  string `json:"stat_value"`
 }
 
-func (q *Queries) GetCricketTournamentMostRuns(ctx context.Context, tournamentID int64) ([]map[string]interface{}, error) {
-	rows, err := q.db.QueryContext(ctx, cricketTournamentsStats, tournamentID)
+func (q *Queries) getPlayerStat(ctx context.Context, query string, tournamentID int64) ([]map[string]interface{}, error) {
+	rows, err := q.db.QueryContext(ctx, query, tournamentID)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to query : ", err)
 	}
 
 	defer rows.Close()
 
-	var mostRuns []map[string]interface{}
+	var playerStats []map[string]interface{}
 	for rows.Next() {
-		var stats mostRun
-		err := rows.Scan(&stats.PlayerID, &stats.PlayerName, &stats.TeamName, &stats.TotalRuns)
+		var stats PlayerStat
+		err := rows.Scan(&stats.PlayerID, &stats.PlayerName, &stats.TeamName, &stats.StatValue)
 		if err != nil {
 			if err == sql.ErrNoRows {
 				return nil, nil
 			}
 			return nil, fmt.Errorf("failed to scan: ", err)
 		}
-		fmt.Println("stats Line no 49: ", stats)
-		mostRuns = append(mostRuns, map[string]interface{}{
+		playerStats = append(playerStats, map[string]interface{}{
 			"player_id":   stats.PlayerID,
 			"player_name": stats.PlayerName,
 			"team_name":   stats.TeamName,
-			"total_runs":  stats.TotalRuns,
+			"stat_value":  stats.StatValue,
 		})
 	}
 
-	return mostRuns, nil
+	return playerStats, nil
+}
 
+const cricketTournamentsMostRuns = `
+	SELECT
+		p.id AS player_id,
+		p.player_name AS player_name,
+		tm.name AS team_name,
+		SUM(b.runs_scored) AS total_runs
+	FROM bats b
+	LEFT JOIN matches m ON m.id = b.match_id
+	LEFT JOIN players p ON p.id = b.batsman_id
+	LEFT JOIN teams tm ON tm.id = b.team_id
+	WHERE m.tournament_id = $1
+	GROUP BY p.id, p.player_name, tm.name
+	HAVING SUM(b.runs_scored) > 0
+	ORDER BY total_runs DESC;
+`
+
+func (q *Queries) GetCricketTournamentMostRuns(ctx context.Context, tournamentID int64) ([]map[string]interface{}, error) {
+	return q.getPlayerStat(ctx, cricketTournamentsMostRuns, tournamentID)
 }
 
 const getCricketTournamentHighestRuns = `
@@ -66,50 +69,17 @@ const getCricketTournamentHighestRuns = `
 		tm.name AS team_name,
 		MAX(b.runs_scored) AS highest_runs
 	FROM bats b
-	JOIN matches m ON m.id = b.match_id
-	JOIN players p ON p.id = b.batsman_id
-	JOIN teams tm ON tm.id = b.team_id
+	LEFT JOIN matches m ON m.id = b.match_id
+	LEFT JOIN players p ON p.id = b.batsman_id
+	LEFT JOIN teams tm ON tm.id = b.team_id
 	WHERE m.tournament_id = $1
 	GROUP BY p.id, p.player_name, tm.name
+	HAVING SUM (b.runs_scored) > 0
 	ORDER BY highest_runs DESC;
 `
 
-type HighestRunsPlayer struct {
-	PlayerID    int64  `json:"player_id"`
-	PlayerName  string `json:"player_name"`
-	TeamName    string `json:"team_name"`
-	HighestRuns string `json:"highest_runs"`
-}
-
 func (q *Queries) GetCricketTournamentHighestRuns(ctx context.Context, tournamentID int64) ([]map[string]interface{}, error) {
-	rows, err := q.db.QueryContext(ctx, getCricketTournamentHighestRuns, tournamentID)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to query : ", err)
-	}
-
-	defer rows.Close()
-
-	var HighestRunsPlayers []map[string]interface{}
-	for rows.Next() {
-		var stats HighestRunsPlayer
-		err := rows.Scan(&stats.PlayerID, &stats.PlayerName, &stats.TeamName, &stats.HighestRuns)
-		if err != nil {
-			if err == sql.ErrNoRows {
-				return nil, nil
-			}
-			return nil, fmt.Errorf("failed to scan: ", err)
-		}
-		fmt.Println("stats Line no 49: ", stats)
-		HighestRunsPlayers = append(HighestRunsPlayers, map[string]interface{}{
-			"player_id":    stats.PlayerID,
-			"player_name":  stats.PlayerName,
-			"team_name":    stats.TeamName,
-			"highest_runs": stats.HighestRuns,
-		})
-	}
-
-	return HighestRunsPlayers, nil
-
+	return q.getPlayerStat(ctx, getCricketTournamentHighestRuns, tournamentID)
 }
 
 const getCricketTournamentMostSixes = `
@@ -119,48 +89,75 @@ const getCricketTournamentMostSixes = `
 		tm.name AS team_name,
 		sum(b.sixes) AS sixes
 	FROM bats b
-	JOIN matches m ON m.id = b.match_id
-	JOIN players p ON p.id = b.batsman_id
-	JOIN teams tm ON tm.id = b.team_id
+	LEFT JOIN matches m ON m.id = b.match_id
+	LEFT JOIN players p ON p.id = b.batsman_id
+	LEFT JOIN teams tm ON tm.id = b.team_id
 	WHERE m.tournament_id = $1
 	GROUP BY p.id, p.player_name, tm.name
+	HAVING SUM(b.sixes) > 0
 	ORDER BY sixes DESC;
 `
 
-type MostSixesByPlayer struct {
-	PlayerID   int64  `json:"player_id"`
-	PlayerName string `json:"player_name"`
-	TeamName   string `json:"team_name"`
-	MostSixes  string `json:"most_sixes"`
+func (q *Queries) GetCricketTournamentMostSixes(ctx context.Context, tournamentID int64) ([]map[string]interface{}, error) {
+	return q.getPlayerStat(ctx, getCricketTournamentMostSixes, tournamentID)
 }
 
-func (q *Queries) GetCricketTournamentMostSixes(ctx context.Context, tournamentID int64) ([]map[string]interface{}, error) {
-	rows, err := q.db.QueryContext(ctx, getCricketTournamentMostSixes, tournamentID)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to query : ", err)
-	}
+const getCricketTournamentMostFours = `
+	SELECT 
+		p.id AS player_id,
+		p.player_name,
+		tm.name AS team_name,
+		sum(b.fours) AS fours
+	FROM bats b
+	LEFT JOIN matches m ON m.id = b.match_id
+	LEFT JOIN players p ON p.id = b.batsman_id
+	LEFT JOIN teams tm ON tm.id = b.team_id
+	WHERE m.tournament_id = $1
+	GROUP BY p.id, p.player_name, tm.name
+	HAVING SUM(b.fours) > 0
+	ORDER BY fours DESC;
+`
 
-	defer rows.Close()
+func (q *Queries) GetCricketTournamentMostFours(ctx context.Context, tournamentID int64) ([]map[string]interface{}, error) {
+	return q.getPlayerStat(ctx, getCricketTournamentMostFours, tournamentID)
+}
 
-	var MostSixesByPlayers []map[string]interface{}
-	for rows.Next() {
-		var stats MostSixesByPlayer
-		err := rows.Scan(&stats.PlayerID, &stats.PlayerName, &stats.TeamName, &stats.MostSixes)
-		if err != nil {
-			if err == sql.ErrNoRows {
-				return nil, nil
-			}
-			return nil, fmt.Errorf("failed to scan: ", err)
-		}
-		fmt.Println("stats Line no 49: ", stats)
-		MostSixesByPlayers = append(MostSixesByPlayers, map[string]interface{}{
-			"player_id":   stats.PlayerID,
-			"player_name": stats.PlayerName,
-			"team_name":   stats.TeamName,
-			"most_sixes":  stats.MostSixes,
-		})
-	}
+const getCricketTournamentMostFifties = `
+	SELECT 
+		p.id AS player_id,
+		p.player_name,
+		tm.name AS team_name,
+		COUNT(*) FILTER(WHERE b.runs_scored >= 50 AND b.runs_scored < 100) AS fifties
+	FROM bats b
+	LEFT JOIN matches m ON m.id = b.match_id
+	LEFT JOIN players p ON p.id = b.batsman_id
+	LEFT JOIN teams tm ON tm.id = b.team_id
+	WHERE m.tournament_id = $1
+	GROUP BY p.id, p.player_name, tm.name
+	HAVING COUNT(*) FILTER(WHERE b.runs_scored >= 50 AND b.runs_scored < 100) > 0
+	ORDER BY fifties DESC;
+`
 
-	return MostSixesByPlayers, nil
+func (q *Queries) GetCricketTournamentMostFifties(ctx context.Context, tournamentID int64) ([]map[string]interface{}, error) {
+	return q.getPlayerStat(ctx, getCricketTournamentMostFifties, tournamentID)
+}
 
+const getCricketTournamentMostHundreds = `
+	SELECT 
+		p.id AS player_id,
+		p.player_name,
+		tm.name AS team_name,
+		COUNT(*) FILTER(WHERE b.runs_scored >= 100) AS hundreds
+	FROM bats b
+	LEFT JOIN matches m ON m.id = b.match_id
+	LEFT JOIN players p ON p.id = b.batsman_id
+	LEFT JOIN teams tm ON tm.id = b.team_id
+	WHERE m.tournament_id = $1
+	GROUP BY p.id, p.player_name, tm.name
+	HAVING COUNT(*) FILTER(WHERE b.runs_scored >= 100) > 0
+	ORDER BY hundreds DESC;
+`
+
+func (q *Queries) GetCricketTournamentMostHundreds(ctx context.Context, tournamentID int64) ([]map[string]interface{}, error) {
+	return q.getPlayerStat(ctx, getCricketTournamentMostHundreds, tournamentID)
 }
