2b0cf24660904ce0f1bc389f5f73a96f06e6595c
Pawan <pc29564530@gmail.com>
2025-07-15 16:21:03 +0530

    Update and refactor the for user_connection

--- api/handlers/follow.go
+++ api/handlers/follow.go
@@ -2,22 +2,22 @@ package handlers
 
 import (
 	"database/sql"
-	db "khelogames/database"
 
 	"khelogames/pkg"
 	"khelogames/token"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
 type createFollowingRequest struct {
-	FollowingOwner string `uri:"following_owner"`
+	TargetPublicID uuid.UUID `uri:"target_public_id"`
 }
 
 // this is function i have to call the get_following endpoint so that using that i can verify the following list
-func (s *HandlersServer) CreateFollowingFunc(ctx *gin.Context) {
+func (s *HandlersServer) CreateUserConnectionsFunc(ctx *gin.Context) {
 	var req createFollowingRequest
 	err := ctx.ShouldBindUri(&req)
 	if err != nil {
@@ -33,13 +33,7 @@ func (s *HandlersServer) CreateFollowingFunc(ctx *gin.Context) {
 	s.logger.Debug("bind the request: ", req)
 	authPayload := ctx.MustGet(pkg.AuthorizationPayloadKey).(*token.Payload)
 
-	arg := db.CreateFollowingParams{
-		FollowerOwner:  authPayload.Username,
-		FollowingOwner: req.FollowingOwner,
-	}
-	s.logger.Debug("params arg: ", arg)
-
-	follower, err := s.store.CreateFollowing(ctx, arg)
+	follower, err := s.store.CreateUserConnections(ctx, authPayload.PublicID, req.TargetPublicID)
 	if err != nil {
 		s.logger.Error("Failed to create following: ", err)
 		ctx.JSON(http.StatusBadRequest, (err))
@@ -53,7 +47,7 @@ func (s *HandlersServer) CreateFollowingFunc(ctx *gin.Context) {
 
 func (s *HandlersServer) GetAllFollowerFunc(ctx *gin.Context) {
 	authPayload := ctx.MustGet(pkg.AuthorizationPayloadKey).(*token.Payload)
-	follower, err := s.store.GetAllFollower(ctx, authPayload.Username)
+	follower, err := s.store.GetAllFollower(ctx, authPayload.PublicID)
 	if err != nil {
 		s.logger.Error("Failed to get follwer: ", err)
 		ctx.JSON(http.StatusInternalServerError, (err))
@@ -66,7 +60,7 @@ func (s *HandlersServer) GetAllFollowerFunc(ctx *gin.Context) {
 
 func (s *HandlersServer) GetAllFollowingFunc(ctx *gin.Context) {
 	authPayload := ctx.MustGet(pkg.AuthorizationPayloadKey).(*token.Payload)
-	follower, err := s.store.GetAllFollowing(ctx, authPayload.Username)
+	follower, err := s.store.GetAllFollowing(ctx, authPayload.PublicID)
 	if err != nil {
 		s.logger.Error("Failed to get following: ", err)
 		ctx.JSON(http.StatusInternalServerError, (err))
@@ -79,7 +73,7 @@ func (s *HandlersServer) GetAllFollowingFunc(ctx *gin.Context) {
 }
 
 type deleteFollowingRequest struct {
-	FollowingOwner string `uri:"following_owner"`
+	TargetPublicID uuid.UUID `uri:"target_public_id"`
 }
 
 func (s *HandlersServer) DeleteFollowingFunc(ctx *gin.Context) {
@@ -100,38 +94,24 @@ func (s *HandlersServer) DeleteFollowingFunc(ctx *gin.Context) {
 
 	authPayload := ctx.MustGet(pkg.AuthorizationPayloadKey).(*token.Payload)
 
-	following, err := s.store.DeleteFollowing(ctx, authPayload.Username, req.FollowingOwner)
+	err = s.store.DeleteUsersConnections(ctx, authPayload.PublicID, req.TargetPublicID)
 	if err != nil {
 		s.logger.Error("Failed to unfollow user: ", err)
 		ctx.JSON(http.StatusInternalServerError, (err))
 		return
 	}
-	s.logger.Debug("successfully get: ", following)
 
-	ctx.JSON(http.StatusOK, following)
+	ctx.JSON(http.StatusOK, gin.H{"deleted": "Unfollow the user"})
 	return
 }
 
-func (s *HandlersServer) CheckConnectionFunc(ctx *gin.Context) {
-
-	followingOwner := ctx.Query("following_owner")
-	authPayload := ctx.MustGet(pkg.AuthorizationPayloadKey).(*token.Payload)
-
-	connectionEstablished, err := s.store.CheckConnection(ctx, authPayload.Username, followingOwner)
-	if err != nil {
-		s.logger.Error("Failed to check connection ", err)
-		ctx.JSON(http.StatusNotFound, err)
-		return
-	}
-	s.logger.Info("Successfully checked connection ")
-	ctx.JSON(http.StatusAccepted, connectionEstablished)
-}
-
 func (s *HandlersServer) IsFollowingFunc(ctx *gin.Context) {
-	followingOnwer := ctx.Query("following_owner")
+	var req struct {
+		TargetPublicID uuid.UUID `uri:"target_public_id"`
+	}
 	authPayload := ctx.MustGet(pkg.AuthorizationPayloadKey).(*token.Payload)
 
-	isFollowing, err := s.store.IsFollowingF(ctx, authPayload.Username, followingOnwer)
+	isFollowing, err := s.store.IsFollowingF(ctx, authPayload.PublicID, req.TargetPublicID)
 	if err != nil {
 		s.logger.Error("Failed to check following ", err)
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to check following status"})
