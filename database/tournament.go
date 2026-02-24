e45f5aa76cafe9c1ef3e6a16b1781bfdebc6497b
Pawan <pc29564530@gmail.com>
2026-02-25 00:15:57 +0530

    refactor: refactor the tournament query to add the profile

--- database/tournament.go
+++ database/tournament.go
@@ -187,10 +187,28 @@ func (q *Queries) GetTournamentsByLevel(ctx context.Context, gameID int64, level
 
 const getTournamentsBySport = `
 SELECT
- JSON_BUILD_OBJECT('id', t.id, 'public_id', t.public_id, 'user_id', t.user_id, 'name', t.name, 'slug', t.slug, 'country', t.country, 'status', t.status, 'level', t.level, 'start_timestamp', t.start_timestamp, 'game_id', t.game_id, 'group_count', t.group_count, 'max_group_team', t.max_group_teams, 'stage', t.stage, 'has_knockout', t.has_knockout)
+JSON_BUILD_OBJECT(
+    'id', t.id,
+    'public_id', t.public_id,
+    'user_id', t.user_id,
+    'name', t.name,
+    'slug', t.slug,
+    'country', t.country,
+    'status', t.status,
+    'level', t.level,
+    'start_timestamp', t.start_timestamp,
+    'game_id', t.game_id,
+    'group_count', t.group_count,
+    'max_group_team', t.max_group_teams,
+    'stage', t.stage,
+    'has_knockout', t.has_knockout,
+    'profile', JSON_BUILD_OBJECT('id', p.id, 'public_id',p.public_id, 'user_public_id',u.public_id,  'username',u.username,  'full_name',u.full_name,  'bio',p.bio,  'avatar_url',p.avatar_url,  'created_at',p.created_at )
+)
 FROM tournaments t
-JOIN games AS g ON g.id = t.game_id
-WHERE t.game_id=$1
+JOIN games g ON g.id = t.game_id
+JOIN user_profiles p ON p.user_id = t.user_id
+JOIN users u ON u.id = t.user_id
+WHERE t.game_id = $1;
 `
 
 type GetTournamentsBySportRow struct {
